2a4604aab7f5bf299dbf1ba8dd3194c6faafa879
Jon Higgs <jhiggs@eml.cc>
2015-02-01 17:12:10 +1100

    Add more functions.

--- keymap_functions.c
+++ keymap_functions.c
@@ -30,17 +30,48 @@ bool guied() {
     return ( get_mods() & MOD_LGUI ) || ( get_mods() & MOD_RGUI );
 }
 
+bool alted() {
+    return ( get_mods() & MOD_LALT ) || ( get_mods() & MOD_RALT );
+}
+
+bool shift_alted() {
+    return (( get_mods() & MOD_LSFT ) || ( get_mods() & MOD_RSFT )) &&
+               (( get_mods() & MOD_LALT ) || ( get_mods() & MOD_RALT ));
+}
+
 void reset_mod_bits(mods) {
     debug("Resetting the mod bits.\n");
+
+    /* Maybe this can be done in a loop */
 
     if (mods & MOD_LSFT ) {
         debug("Turning LSFT back on.");
         add_mods(MOD_BIT(KC_LSFT));
     }
 
+    if (mods & MOD_RSFT ) {
+        debug("Turning RSFT back on.");
+        add_mods(MOD_BIT(KC_RSFT));
+    }
+
     if (mods & MOD_LCTL ) {
         debug("Turning LCTL back on.");
         add_mods(MOD_BIT(KC_LCTL));
+    }
+
+    if (mods & MOD_RCTL ) {
+        debug("Turning RCTL back on.");
+        add_mods(MOD_BIT(KC_RCTL));
+    }
+
+    if (mods & MOD_LALT ) {
+        debug("Turning LALT back on.");
+        add_mods(MOD_BIT(KC_LALT));
+    }
+
+    if (mods & MOD_RALT ) {
+        debug("Turning RALT back on.");
+        add_mods(MOD_BIT(KC_RALT));
     }
 }
 
@@ -79,5 +110,78 @@ void ctrl_h_is_backspace(bool pressed) {
         del_key(KC_BSPC);
         del_key(KC_H);
         send_keyboard_report();
+    }
+}
+
+void ctrl_a_is_home(bool pressed) {
+    uint8_t mods = get_mods();
+    if (pressed) {
+        if ( controlled() ) {
+            del_mods(MOD_BIT(KC_LCTRL));
+            clear_mods();
+            add_key(KC_HOME);
+            send_keyboard_report();
+            reset_mod_bits(mods);
+        } else {
+            add_key(KC_A);
+            send_keyboard_report();
+        }
+    } else {
+        // release the keys.
+        del_key(KC_HOME);
+        del_key(KC_A);
+        send_keyboard_report();
+    }
+}
+
+void tmux(bool pressed) {
+    if (pressed) {
+        add_mods(MOD_BIT(KC_LALT));
+        add_key(KC_COMMA);
+        send_keyboard_report();
+        clear_keyboard();
+        send_keyboard_report();
+    }
+}
+
+void del_word(bool pressed) {
+    uint8_t mods = get_mods();
+
+    if (event.pressed) {
+        if ( shift_alted() ) {
+            /* forward delete word. */
+            /* ESC, D */
+            clear_mods();
+            add_mods(MOD_BIT(KC_LALT));
+            add_key(KC_ESC);
+            send_keyboard_report();
+            add_key(KC_D);
+            send_keyboard_report();
+            reset_mod_bits(mods)
+        } else if ( alted() ) {
+            /* backwards delete word. */
+            /* ESC, BSPC */
+            del_mods(MOD_BIT(KC_LALT));
+            add_key(KC_ESC);
+            send_keyboard_report();
+            del_key(KC_ESC);
+            send_keyboard_report();
+            add_key(KC_BSPC);
+            send_keyboard_report();
+            reset_mod_bits(mods)
+        } else if ( shifted() ) {
+            /* forward delete. */
+            /* DELETE */
+            clear_mods();
+            add_key(KC_DELETE);
+            send_keyboard_report();
+            reset_mod_bits(mods)
+        } else {
+            // backwards delete
+            add_key(KC_BSPC);
+            send_keyboard_report();
+        }
+    } else {
+        clear_keyboard_but_mods();
     }
 }
