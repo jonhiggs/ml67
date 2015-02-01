931c7937535943803c1123e89e4444dd021bc866
Jon Higgs <jhiggs@eml.cc>
2015-02-01 17:12:21 +1100

    Strip the special stuff out of keymap standard.c

--- keymap_standard.c
+++ keymap_standard.c
@@ -1,5 +1,4 @@
 #include "keymap_common.h"
-#include "keymap_functions.c"
 
 /* KEYMAP
     K00,K01, K02, K03, K04, K05, K06, K07, K08, K09, K0A, K0B, K0C, K0D, K0E,\
@@ -24,9 +23,9 @@
 const uint8_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     /* 0: plain */
     KEYMAP(   // LAYER 0: Default
-      FN10, 1,    2,    3,    4,    5,    6,    7,    8,    9,    0,    MINS, EQL,  BSPC, GRV, \
+      ESC,  1,    2,    3,    4,    5,    6,    7,    8,    9,    0,    MINS, EQL,  BSPC, GRV, \
       TAB,  Q,    W,    E,    R,    T,    Y,    U,    I,    O,    P,    LBRC, RBRC, BSLS,      \
-      CAPS, A,    S,    D,    F,    G,    FN13, J,    K,    L,    SCLN, QUOT,       ENT,       \
+      CAPS, A,    S,    D,    F,    G,    H,    J,    K,    L,    SCLN, QUOT,       ENT,       \
       LSFT, Z,    X,    C,    V,    B,    N,    M,    COMM, DOT,  SLSH,       RSFT, UP,   DEL, \
       LCTL, LGUI, LALT, FN1,              SPC,              FN1,  RALT, RGUI, LEFT, DOWN, RGHT \
     ),
@@ -39,134 +38,9 @@ const uint8_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     ),
 };
 
-/* id for user defined functions */
-enum function_id {
-    CTRL_H,
-    ESC,
-    HOME,
-    SPACE,
-    SPOTLIGHT,
-    TMUX,
-    DEL_WORD,
-};
-
 /*
  * Fn action definition
  */
 const uint16_t PROGMEM fn_actions[] = {
     [1] = ACTION_LAYER_MOMENTARY(1),          // FN1 switch to layer 1
-    [2] = ACTION_LAYER_MOMENTARY(2),          // FN2 switch to layer 2
-    [10] = ACTION_FUNCTION(ESC),              // Special ESC key.
-    [11] = ACTION_FUNCTION(SPACE),            // Special Space Key.
-    [13] = ACTION_FUNCTION(CTRL_H),           // Ctrl-H sends backspace.
-    [14] = ACTION_FUNCTION(HOME),             // Ctrl-A sends home.
-    [15] = ACTION_FUNCTION(TMUX),             // tmux bind prefix
-    [16] = ACTION_FUNCTION(DEL_WORD),         // Alt-Backspace deletes a word.
 };
-
-void action_function(keyrecord_t *record, uint8_t id, uint8_t opt)
-{
-  keyevent_t event = record->event;
-  tap_t tap = record->tap;
-
-  switch (id) {
-    case CTRL_H:
-        ctrl_h_is_backspace(event.pressed);
-        break;
-
-    case ESC:
-        shift_esc_is_tilde(event.pressed);
-        break;
-
-    case HOME:
-      if (event.pressed) {
-        // press the keys
-        if (get_mods() & MOD_LCTL) {
-          del_mods(MOD_BIT(KC_LCTRL));
-          add_key(KC_HOME);
-          send_keyboard_report();
-          add_mods(MOD_BIT(KC_LCTRL));
-        } else {
-          add_key(KC_A);
-          send_keyboard_report();
-        }
-      } else {
-        // release the keys.
-        del_key(KC_HOME);
-        del_key(KC_A);
-        send_keyboard_report();
-      }
-      break;
-
-    case SPACE:
-      if (event.pressed) {
-        // press the keys
-        if (get_mods() & MOD_LCTL) {
-          del_mods(MOD_BIT(KC_LCTRL));
-          add_key(KC_SPC);
-          send_keyboard_report();
-          add_mods(MOD_BIT(KC_LCTRL));
-         } else {
-          add_key(KC_SPC);
-          send_keyboard_report();
-        }
-      } else {
-        // release the keys.
-        del_key(KC_SPC);
-        send_keyboard_report();
-      }
-      break;
-
-    case TMUX:
-      if (event.pressed) {
-        add_mods(MOD_BIT(KC_LALT));
-        add_key(KC_COMMA);
-        send_keyboard_report();
-        del_key(KC_COMMA);
-        send_keyboard_report();
-        del_mods(MOD_BIT(KC_LALT));
-        send_keyboard_report();
-      }
-      break;
-
-    case DEL_WORD:
-      if (event.pressed) {
-        if (get_mods() & MOD_LALT) {
-          del_mods(MOD_BIT(KC_LALT));
-          add_key(KC_ESC);
-          send_keyboard_report();
-          del_key(KC_ESC);
-          send_keyboard_report();
-          if (get_mods() & MOD_LSFT) {
-            // with shift, forward delete word
-            del_mods(MOD_BIT(KC_LSFT));
-            add_key(KC_D);
-            send_keyboard_report();
-            add_mods(MOD_BIT(KC_LSFT));
-            send_keyboard_report();
-          } else {
-            // without shift, backwards delete word
-            add_key(KC_BSPC);
-            send_keyboard_report();
-          }
-          add_mods(MOD_BIT(KC_LALT));
-          send_keyboard_report();
-        } else if (get_mods() & MOD_LSFT) {
-          del_mods(MOD_BIT(KC_LSHIFT));
-          add_key(KC_DELETE);
-          add_mods(MOD_BIT(KC_LSHIFT));
-          send_keyboard_report();
-        } else {
-          add_key(KC_BSPC);
-          send_keyboard_report();
-        }
-      } else {
-        del_key(KC_BSPC);
-        del_key(KC_D);
-        del_key(KC_DELETE);
-        send_keyboard_report();
-      }
-      break;
-  }
-
-}
