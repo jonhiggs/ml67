f8a55318559e1d29cfdedcf6ba66b30bb83392cd
Jon Higgs <jhiggs@eml.cc>
2015-02-01 11:56:40 +1100

    Cleanup keymap_common.c a little bit.

--- keymap_common.c
+++ keymap_common.c
@@ -16,15 +16,14 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 #include "keymap_common.h"
 
-
 /* translates key to keycode */
-uint8_t keymap_key_to_keycode(uint8_t layer, keypos_t key)
-{
+uint8_t keymap_key_to_keycode(uint8_t layer, keypos_t key) {
     return pgm_read_byte(&keymaps[(layer)][(key.row)][(key.col)]);
 }
 
 /* translates Fn keycode to action */
-action_t keymap_fn_to_action(uint8_t keycode)
-{
-    return (action_t){ .code = pgm_read_word(&fn_actions[FN_INDEX(keycode)]) };
+action_t keymap_fn_to_action(uint8_t keycode) {
+    return (action_t) {
+        .code = pgm_read_word(&fn_actions[FN_INDEX(keycode)])
+    };
 }
