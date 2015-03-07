7fbfd88f6811fd11317e31a3258af4a19e0408ca
Jon Higgs <jhiggs@eml.cc>
2015-03-07 19:45:30 +1100

    Add missing ,

--- keymap_common.h
+++ keymap_common.h
@@ -37,7 +37,7 @@ extern const uint16_t fn_actions[];
     K10, K11, K12, K13, K14, K15, K16, K17, K18, K19, K1A, K1B, K1C, K1D,    \
     K20,   K21, K22, K23, K24, K25, K26, K27, K28, K29, K2A, K2B, K2C,       \
     K30,     K31, K32, K33, K34, K35, K36, K37, K38, K39, K3A, K3B, K3C, K3D,\
-    K40, K41, K42, K43,           K44,          K45, K46, K47, K48, K49, K4A \
+    K40, K41, K42, K43,           K44,          K45, K46, K47, K48, K49, K4A,\
     K50, K51                                                                 \
 ) { \
     { KC_##K11 , KC_##K10 , KC_##K21 , KC_##K00 , KC_##K31 , KC_##K4A , KC_##K0D , KC_##K01 , KC_NO    },\
