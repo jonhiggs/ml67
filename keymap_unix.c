197cc075456c0d2dc3307d207fdb896d11888348
Jon Higgs <jhiggs@eml.cc>
2015-03-18 17:29:16 +1100

    I think CTRL_A should be CTRL_A.

--- keymap_unix.c
+++ keymap_unix.c
@@ -7,7 +7,7 @@ const uint8_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KEYMAP(   // LAYER 0: Default
       FN13, 1,    2,    3,    4,    5,    6,    7,    8,    9,    0,    MINS, EQL,  BSLS, GRV, \
       TAB,  Q,    W,    E,    R,    T,    Y,    U,    I,    O,    P,    LBRC, RBRC, FN10,      \
-      LCTL, FN12, S,    D,    F,    G,    FN11, J,    K,    L,    SCLN, QUOT,       ENT,       \
+      LCTL, A,    S,    D,    F,    G,    FN11, J,    K,    L,    SCLN, QUOT,       ENT,       \
       LSFT, Z,    X,    C,    V,    B,    N,    M,    COMM, DOT,  SLSH,       RSFT, UP,   NO , \
       FN14, NO,   FN1,  LGUI,             SPC,              RALT,  FN2, NO,   LEFT, DOWN, RGHT,\
       LGUI, LGUI                                                                               \
